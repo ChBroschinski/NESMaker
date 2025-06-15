d177539b261c13c4cdddc583ef40aa665a2348f1
ChBroschinski <chi.broschinski@gmail.com>
2025-06-15 15:44:03 +0200

    final gitignore fix frfr

---
+++ NESmaker_4_5_x/GameEngineData/Routines/BASE_4_5/Game/inputScripts/moveUp.asm
@@ -0,0 +1,27 @@
+;;;; 
+
+    ;;; code adjustments taken from: https://www.nesmakers.com/index.php?threads/weapon-check-destroy-weapon-4-5-9-solved.8085/
+    
+    ;If the sword object is on-screen we can't move NEW CODE
+    CountObjectType #1 ;;; Sword object
+    BEQ +
+    RTS
+    + 
+
+    TXA
+    STA temp ;; assumes the object we want to move is in x.
+  GetActionStep temp
+    CMP #$02 ;attack
+    BNE +notAttacking
+        RTS
+    +notAttacking
+    CMP #$07
+    BNE +notHurt
+        RTS
+    +notHurt
+        StartMoving temp, #UP
+        TXA
+        STA temp ;; assumes the object we want to move is in x.
+        ChangeFacingDirection temp, #FACE_UP
+ 
+    RTS
